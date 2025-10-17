b32b04ebfa675b0b79fc4920b684b7686aad6df8
krishna <krishna04.b@gamil.com>
2025-10-17 02:32:29 +0000

    created-by-krishna

---
+++ Myapp.java
@@ -0,0 +1,8 @@
+public class Myapp {
+	public static void main(String[] args) {
+		int a=10, int b=20;
+		System.out.println("ADdition is:+(a+b));
+		System.out.println("difference is:+(a-b));
+	}
+}
+
